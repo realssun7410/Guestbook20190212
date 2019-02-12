0e90eabd6035e74ab34d7e0494ab80baa899ee59
Hyejin Park <hyejin.park@one-line.com>
2019-02-12 09:47:07 +0900

    전체구조 완성

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBAsiy7dPo_4xVty440snA-Sun7-PrXDNY",
+    authDomain: "gbook1line.firebaseapp.com",
+    databaseURL: "https://gbook1line.firebaseio.com",
+    projectId: "gbook1line",
+    storageBucket: "gbook1line.appspot.com",
+    messagingSenderId: "731905230056"
+  };
+  firebase.initializeApp(config);

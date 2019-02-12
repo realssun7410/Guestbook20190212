e07e35ab6c4a74c1cc3deb3ef4f30a9cfa3f79c4
Hyejin Park <hyejin.park@one-line.com>
2019-02-12 11:44:47 +0900

    database 연결

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,7 @@ var log = console.log;
 var auth = firebase.auth();
 var db = firebase.database();
 var googleAuth = new firebase.auth.GoogleAuthProvider();
+var ref = null;
 
 /***** Auth *****/
 $("#login_bt").on("click", function () {
@@ -25,14 +26,31 @@ $("#logout_bt").on("click", function () {
 auth.onAuthStateChanged(function (result) {
   if (result) {
     log(result);
-    var email = '<img src="'+result.photoURL+'" style="width:24px; border-radius:50%;"> '+result.email;
+    var email = '<img src="' + result.photoURL + '" style="width:24px; border-radius:50%;"> ' + result.email;
     $("#login_bt").hide();
     $("#logout_bt").show();
     $("#user_email").html(email);
-  }
-  else {
+  } else {
     $("#login_bt").show();
     $("#logout_bt").hide();
     $("#user_email").html('');
   }
 });
+
+/***** Database *****/
+init();
+
+function init() {
+  ref = db.ref("root/gbook");
+  ref.on("child_added", onAdded);
+}
+
+function onAdded(data) {
+  log(data);
+}
+ref = db.ref("root/gbook");
+ref.push({
+  content: "테스트",
+  writer: "홍길동",
+  wtime: Date.now()
+}).key;

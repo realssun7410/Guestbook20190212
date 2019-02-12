b0bd976d75165bb248112c14ca0fba2dcf37e931
Hyejin Park <hyejin.park@one-line.com>
2019-02-12 10:51:30 +0900

    로그인 로그아웃

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,38 @@
 var config = {
-    apiKey: "AIzaSyBAsiy7dPo_4xVty440snA-Sun7-PrXDNY",
-    authDomain: "gbook1line.firebaseapp.com",
-    databaseURL: "https://gbook1line.firebaseio.com",
-    projectId: "gbook1line",
-    storageBucket: "gbook1line.appspot.com",
-    messagingSenderId: "731905230056"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBAsiy7dPo_4xVty440snA-Sun7-PrXDNY",
+  authDomain: "gbook1line.firebaseapp.com",
+  databaseURL: "https://gbook1line.firebaseio.com",
+  projectId: "gbook1line",
+  storageBucket: "gbook1line.appspot.com",
+  messagingSenderId: "731905230056"
+};
+firebase.initializeApp(config);
+
+/***** 전역변수 설정 *****/
+var log = console.log;
+var auth = firebase.auth();
+var db = firebase.database();
+var googleAuth = new firebase.auth.GoogleAuthProvider();
+
+/***** Auth *****/
+$("#login_bt").on("click", function () {
+  auth.signInWithPopup(googleAuth);
+});
+$("#logout_bt").on("click", function () {
+  auth.signOut();
+})
+
+auth.onAuthStateChanged(function (result) {
+  if (result) {
+    log(result);
+    var email = '<img src="'+result.photoURL+'" style="width:24px; border-radius:50%;"> '+result.email;
+    $("#login_bt").hide();
+    $("#logout_bt").show();
+    $("#user_email").html(email);
+  }
+  else {
+    $("#login_bt").show();
+    $("#logout_bt").hide();
+    $("#user_email").html('');
+  }
+});

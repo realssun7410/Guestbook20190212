5e9a4826a5c4a9ff285f85089ff3f47f3efbdce7
Hyejin Park <hyejin.park@one-line.com>
2019-02-12 13:49:06 +0900

    write 작업중

--- public/js/index.js
+++ public/js/index.js
@@ -14,6 +14,7 @@ var auth = firebase.auth();
 var db = firebase.database();
 var googleAuth = new firebase.auth.GoogleAuthProvider();
 var ref = null;
+var user = null;
 
 /***** Auth *****/
 $("#login_bt").on("click", function () {
@@ -25,12 +26,13 @@ $("#logout_bt").on("click", function () {
 
 auth.onAuthStateChanged(function (result) {
   if (result) {
-    log(result);
+    user = result;
     var email = '<img src="' + result.photoURL + '" style="width:24px; border-radius:50%;"> ' + result.email;
     $("#login_bt").hide();
     $("#logout_bt").show();
     $("#user_email").html(email);
   } else {
+    user = null;
     $("#login_bt").show();
     $("#logout_bt").hide();
     $("#user_email").html('');
@@ -46,11 +48,31 @@ function init() {
 }
 
 function onAdded(data) {
-  log(data);
+  var k = data.key;
+  var v = data.val();
+  var html = '<ul id="'+k+'" data-uid="'+v.uid+'" class="gbook">';
+  html += '<li>'+v.uname+' ('+v.email+')</li>';
+  html += '<li>'+v.content+'</li>';
+  html += '<li>'+v.wdate+'</li>';
+  html += '</ul>';
+  $(".gbooks").prepend(html);
 }
-ref = db.ref("root/gbook");
-ref.push({
-  content: "테스트",
-  writer: "홍길동",
-  wtime: Date.now()
-}).key;
+
+$("#save_bt").on("click", function(){
+var $content = $("#content");
+if($content.val() == ""){
+  alert("내용을 입력하세요.");
+  $content.focus();
+}
+else{
+  ref = db.ref("root/gbook/");
+  ref.push({
+    email: user.email,
+    uid: user.uid,
+    uname: user.displayName,
+    content: $content.val(),
+    wdate: Date.now()
+  }).key;
+  $content.val('');
+}
+});
